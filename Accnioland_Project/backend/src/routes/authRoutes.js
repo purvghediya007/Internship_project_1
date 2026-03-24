be13717c27cfc4123b750259fcb4439fc2c345b9
purvghediya007 <ghediyapurv99@gmail.com>
2026-03-24 13:07:21 +0530

    Modify login route to include logging

--- Accnioland_Project/backend/src/routes/authRoutes.js
+++ Accnioland_Project/backend/src/routes/authRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { register, login } = require("../controllers/authController");
 
 router.post("/register", register);
-router.post("/login", login);
-
+// router.post("/login", login);
+router.post("/login", (req, res) => {
+  console.log("LOGIN API HIT");
+  login(req, res);
+});
 module.exports = router;
